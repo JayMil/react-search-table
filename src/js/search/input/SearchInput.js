1327656c880fd4b71b9bd0f5877ffad6833fd809
Jake <milder.jake@gmail.com>
2016-04-06 20:50:05 -0500

    Updated to Airbnb standards

--- src/js/search/input/SearchInput.js
+++ src/js/search/input/SearchInput.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-
+SearchInput.propTypes = {
+    data: React.PropTypes.array.isRequired,
+    columns: React.PropTypes.object.isRequired
+};
 //Display rows of data based on data provided.
-export default function(props) {
-
+export default function SearchInput() {
     return (
-        <div class="form-group">
-            <label for="searchInput" class="sr-only">Search</label>
-            <input type="text" id="searchInput" class="form-control search-input" placeholder="Search"/>
-            <label for="searchInput" class="fa fa-search fa-lg search-icon"></label>
+        <div className="form-group">
+            <label htmlFor="searchInput" className="sr-only">Search</label>
+            <input type="text" id="searchInput" className="form-control search-input" placeholder="Search"/>
+            <label htmlFor="searchInput" className="fa fa-search fa-lg search-icon"></label>
         </div>
     );
 
